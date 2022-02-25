ccc223e69d403fab550ef6c6af6749f8fd87e6e6
Marshall Jones <marshall@offby3.com>
2022-02-24 15:58:56 -1000

    Update info.js

--- src/objects/info.js
+++ src/objects/info.js
@@ -8,27 +8,8 @@ function parse(info){
 
         res.push(`--- \n`)
 
-        // Set the title of the doc
-        if ('title' in info) {
-            res.push(`title: ${info.title} \n`)
-        }
-
-        // Set the language tab
-        res.push(`language_tabs: \n   - shell \n`)
-
-        // Set the footer
-        res.push(`toc_footers: \n   - <a href='#'>Sign Up for a Developer Key</a> \n   - <a href='https://github.com/lavkumarv'>Documentation Powered by lav</a> \n`)
-        
-        // Include the error codes
-        res.push(`includes: \n   - errors \n`)
-
-        // Include the search options
-        res.push(`search: true \n`)
-
-        res.push(`--- \n`)
-
         // Introduction part
-        res.push(`# Introduction \n`)
+        res.push(`# ${info.title} \n`)
 
         if ('description' in info) {
             res.push(`${info.description} \n`) 
